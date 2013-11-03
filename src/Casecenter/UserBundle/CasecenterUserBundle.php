137d8a5c40b9bf9663f3562e16df3a083c3d1f00
Adrien <manteletad@gmail.com>
2013-11-03 12:57:13 +0100

    Création du bundle User

---
+++ src/Casecenter/UserBundle/CasecenterUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Casecenter\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CasecenterUserBundle extends Bundle
+{
+}
---
+++ src/Casecenter/UserBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Casecenter\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('CasecenterUserBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
